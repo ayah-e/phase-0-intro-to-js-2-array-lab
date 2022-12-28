26ae1b14fe9e57d5e65ff4a378f931ffc9e507c7
Ayah Elsais <elsais.ayah@gmail.com>
2022-12-28 17:26:19 -0500

    commit tree edd44e1bbfe8c71c82f9aca6621721dfb52a014a

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution here!
+const cats = [ 
+    "Milo",
+    "Otis",
+    "Garfield"
+];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const copyCats = [...cats];
+    copyCats.push(name);
+    return copyCats;
+}
+
+function prependCat(name) {
+    const copyCats = [...cats];
+    copyCats.unshift(name);
+    return copyCats;
+}
+
+function removeLastCat() {
+    const copyCats = [...cats];
+    copyCats.pop();
+    return copyCats;
+}
+
+function removeFirstCat() {
+    const copyCats = [...cats];
+    copyCats.shift();
+    return copyCats;
+}
